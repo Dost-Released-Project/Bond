f41f04918a8a72ca5b4e3bac11da400b8b3083dc
Moon <https://github.com/mhs0209>
2026-04-26 22:58:52 +0900

    [프리팹 폴더 추가]

--- Assets/97. Moon/V2(Inventory, Building, Supply)/Scripts/GetLiftimeScope.cs
+++ Assets/97. Moon/V2(Inventory, Building, Supply)/Scripts/GetLiftimeScope.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
-public class GameLifetimeScope : LifetimeScope
+public class GetLifetimeScope : LifetimeScope
 {
     protected override void Configure(IContainerBuilder builder)
     {
         // Manager & Service (싱글톤처럼 유지)
         // 인벤토리
-        builder.Register<TotalInventory>(Lifetime.Singleton).WithParameter("capacity", 16).AsImplementedInterfaces().AsSelf();
-        builder.Register<ExpeditionInventory>(Lifetime.Singleton).WithParameter("capacity", 2).AsImplementedInterfaces().AsSelf();
+        builder.Register<TotalInventory>(Lifetime.Singleton).WithParameter("Capacity", 16).AsImplementedInterfaces().AsSelf();
+        builder.Register<ExpeditionInventory>(Lifetime.Singleton).WithParameter("Capacity", 2).AsImplementedInterfaces().AsSelf();
         builder.Register<InventoryTransferService>(Lifetime.Singleton);
         // 자원 및 건물
         builder.Register<ResourceManager>(Lifetime.Singleton);
