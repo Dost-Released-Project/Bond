7be368b07ad56bec933a0dddc76dc2eb6b33cffb
gyusummer <gyusummer@gmail.com>
2026-05-21 00:30:27 +0900

    성향 클래스 수정

--- Assets/97. Moon/V1/Scripts/Trait.cs
+++ Assets/97. Moon/V1/Scripts/Trait.cs
@@ -2,12 +2,21 @@ using Reactions;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+public enum E_TraitType
+{
+    None,
+    Positive,
+    Neutral,
+    Negative,
+}
+
 [System.Serializable]
 public class Trait
 {
+    public E_TraitType Type;
     public string Name = "Missing";
     [TextArea] public string Description = "Missing";
-    // public Trigger Trigger;  // 성향에 고정된 트리거
+    // public Reaction Reaction;  // 성향에 고정된 리액션
 
     public override string ToString()
     {
