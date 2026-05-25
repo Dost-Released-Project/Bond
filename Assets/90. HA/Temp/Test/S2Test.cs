3b5f710f19ffd6a218d3c4da60b2d5bac6bcd3e7
gyusummer <gyusummer@gmail.com>
2026-05-25 15:37:43 +0900

    테스트 코드 수정

--- Assets/90. HA/Temp/Test/S2Test.cs
+++ Assets/90. HA/Temp/Test/S2Test.cs
@@ -1,4 +1,5 @@
 using Bond.Expedition;
+using Bond.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -8,20 +9,6 @@ namespace _90._HA.Temp.Test
 {
     public class S2Test : MonoBehaviour
     {
-        [Inject] ExpeditionPayload payload;
-
-        private void Update()
-        {
-            if (Keyboard.current.numpad0Key.wasPressedThisFrame)
-            {
-                Debug.Log(payload);
-                
-            }
-            
-            if (Keyboard.current.numpad9Key.wasPressedThisFrame)
-            {
-                SceneManager.LoadSceneAsync("Town");
-            }
-        }
+        
     }
 }
